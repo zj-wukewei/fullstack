734629baaeb9b827dabcb946a381800d90f026d5
wukewei <565389849@qq.com>
2019-11-30 12:56:07 +0800

    格式化

--- client/src/layouts/index.js
+++ client/src/layouts/index.js
@@ -10,56 +10,50 @@ import './index.less';
 
 const { Header, Content } = Layout;
 const EXCHANGE_WHOAMI = gql`
-  query WhoAmI  {
+  query WhoAmI {
     whoAmI {
-       id
-       phone
-        info {
-         id
-         name
-         address
-         age
+      id
+      phone
+      info {
+        id
+        name
+        address
+        age
       }
     }
   }
 `;
-
 
 function BasicLayout(props) {
   const { loading, data } = useQuery(EXCHANGE_WHOAMI);
 
   const menu = (
     <Menu>
-     <Menu.Item>
-       <div onClick={() => loginOut()} style={{ width: '100px', textAlign: 'center' }}>
-         退出
-       </div>
+      <Menu.Item>
+        <div onClick={() => loginOut()} style={{ width: '100px', textAlign: 'center' }}>
+          退出
+        </div>
       </Menu.Item>
     </Menu>
   );
 
-  console.log('1111', loading)
-
   return (
     <Spin spinning={loading} tip="初始化中...">
-    <Layout className='layout'>
-    <Header>
-      <div className="logo" />
-     
-     <Dropdown overlay={menu} placement="bottomLeft">
-      <div className='user-info'>
-        {data && data.whoAmI && data.whoAmI.info && data.whoAmI.info.name}
-      </div>
-    </Dropdown>
-      
-    </Header>
-    <Content style={{ padding: '10px' }}>
-      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-        {props.children}
-      </div>
-    </Content>
-  </Layout>
-  </Spin>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+
+          <Dropdown overlay={menu} placement="bottomLeft">
+            <div className="user-info">
+              {data && data.whoAmI && data.whoAmI.info && data.whoAmI.info.name}
+            </div>
+          </Dropdown>
+        </Header>
+        <Content style={{ padding: '10px' }}>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>{props.children}</div>
+        </Content>
+      </Layout>
+    </Spin>
   );
 }
 
